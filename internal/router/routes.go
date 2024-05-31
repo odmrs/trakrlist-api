fcde572faa60123d2d645e853851d2e9f14de394
Marcos Vinicius <omarcosviniciusdev@gmail.com>
2024-05-31 01:16:49 -0300

    feat: put handlers functions in a different file

--- internal/router/routes.go
+++ internal/router/routes.go
@@ -1,32 +1,18 @@
 package router
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
+
+	"github.com/odmrs/trakrlist-api/internal/handler"
 )
 
 func initializeRoutes(v1 *http.ServeMux) {
-	v1.HandleFunc("GET /api/v1/ping", ping)
+	v1.HandleFunc("GET /api/v1/ping", handler.Ping)
 
-	v1.HandleFunc("GET /api/v1/content", ping)
-	v1.HandleFunc("POST /api/v1/content", ping)
-	v1.HandleFunc("PUT /api/v1/content", ping)
-	v1.HandleFunc("DELETE /api/v1/content", ping)
-	v1.HandleFunc("GET /api/v1/contents", ping)
-}
-
-func ping(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	pong := map[string]string{
-		"message": "pong",
-	}
-
-	pongJson, err := json.Marshal(pong)
-	if err != nil {
-		fmt.Println("marshal error")
-	}
-
-	fmt.Fprint(w, string(pongJson))
+	// Content handlers
+	v1.HandleFunc("GET /api/v1/content/{content}", handler.Ping)
+	v1.HandleFunc("POST /api/v1/content", handler.Ping)
+	v1.HandleFunc("PUT /api/v1/content", handler.Ping)
+	v1.HandleFunc("DELETE /api/v1/content", handler.Ping)
+	v1.HandleFunc("GET /api/v1/contents", handler.Ping)
 }
