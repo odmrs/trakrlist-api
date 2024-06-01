ae11faddb1fdfa2c45c9dcdf76c1d38273f012d5
Marcos Vinicius <omarcosviniciusdev@gmail.com>
2024-06-01 16:13:30 -0300

    feat: using logger everywhere

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"github.com/odmrs/trakrlist-api/config"
 	"github.com/odmrs/trakrlist-api/internal/router"
 )
 
+var logger config.Logger
+
 func main() {
+	logger = *config.GetLogger("main")
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+	// Initialize Router
 	router.Initialize()
 }
