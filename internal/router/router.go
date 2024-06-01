75e1e7a5dbafa2ebe6eba65577866cdde6ba9397
Marcos Vinicius <omarcosviniciusdev@gmail.com>
2024-06-01 16:13:59 -0300

    refactor: using own logger inside router

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,17 +1,21 @@
 package router
 
 import (
-	"log"
 	"net/http"
+
+	"github.com/odmrs/trakrlist-api/config"
 )
 
+var logger config.Logger
+
 func Initialize() {
+	logger = *config.GetLogger("router")
 	v1 := http.NewServeMux()
 
 	initializeRoutes(v1)
 
 	server := &http.Server{Addr: "localhost:8080", Handler: v1}
 
-	log.Print("Listening...")
+	logger.Info("Listening...")
 	server.ListenAndServe()
 }
