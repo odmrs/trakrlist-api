2f8532fe43b609979dea70243a19439ca46c5e98
Marcos Vinicius <omarcosviniciusdev@gmail.com>
2024-06-06 22:21:49 -0300

    feat: create show all content from database

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -11,6 +11,15 @@ var (
 	db     *sql.DB
 )
 
+type Content struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Type     string `json:"type"`
+	Genres   string `json:"genres"`
+	Author   string `json:"author"`
+	Duration int    `json:"duration"`
+}
+
 func InitializeHandler() {
 	logger = config.GetLogger("handler")
 	db, _ = config.InitializePostgres()
--- internal/handler/showListContentsHandler.go
+++ internal/handler/showListContentsHandler.go
@@ -1,1 +1,47 @@
 package handler
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func ListAllContentHandler(w http.ResponseWriter, r *http.Request) {
+	content, err := fetchContent(db)
+	if err != nil {
+		logger.Errorf("fetch content to json error: %v", err)
+	}
+
+	jsonData, err := json.Marshal(content)
+	if err != nil {
+		logger.Errorf("marshal to json content error: %v", err)
+	}
+
+	fmt.Println(string(jsonData))
+	sendSuccess(w, "list-content", content)
+}
+
+func fetchContent(db *sql.DB) ([]Content, error) {
+	var contents []Content
+
+	rows, err := db.Query("SELECT id, name, type, genres, author, duration FROM content")
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var content Content
+		if err := rows.Scan(&content.ID, &content.Name, &content.Type, &content.Genres, &content.Author, &content.Duration); err != nil {
+			return nil, err
+		}
+
+		contents = append(contents, content)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return contents, nil
+}
